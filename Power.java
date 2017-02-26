4fca222c594af687a8e095f2d9316272a25bbd2d
PupilDark <sdong@uwm.edu>
2017-02-25 20:50:26 -0600

    Add files via upload

--- Power.java
+++ Power.java
@@ -1,10 +1,18 @@
 
 public class Power {
-	private boolean powerStatus;
+	boolean powerStatus;
 	
 	public Power(){
 		powerStatus = false; //default value
+	}
+	
+	public void powerOn(){
 		if (!powerStatus) powerStatus = true;
+		else System.out.println("Power is already On!");
+	}
+	
+	public void powerOff(){
 		if (powerStatus) powerStatus = false;
+		else System.out.println("Power is already Off!");
 	}
 }
